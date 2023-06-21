0f21ec4e3b1b7d3508148ccbd4c13f782aa09ea1
David Laessker <davidlaessker@gmail.com>
2023-06-21 10:58:17 +0200

    cleaner ui

--- script/window_config.py
+++ script/window_config.py
@@ -1,13 +1,12 @@
 import tkinter as tk
-import ttkthemes
+from tkinter import ttk
 from ttkthemes import ThemedStyle
 
-
-
-# window_config.py
-
-import tkinter as tk
-from ttkthemes import ThemedStyle
+def clear_placeholder(event):
+    """Function to clear the placeholder text when input_box receives focus."""
+    input_box = event.widget
+    if input_box.get() == 'Enter your message...':
+        input_box.delete(0, 'end')
 
 def configure_window(window, browse_file):
     window_width = 600
@@ -20,40 +19,39 @@ def configure_window(window, browse_file):
 
     # Apply a theme
     style = ThemedStyle(window)
-    style.theme_use("clam")  # Example theme name
+    style.theme_use("yaru")  # Modern dark theme
 
     # Define colors and styles
-    window.configure(bg="#f0f0f0")
+    window.configure(bg="#2d2d2d")
+    window.title('My Application') # Add a title to your window
     window.option_add("*Font", "Arial 12")
-    window.option_add("*Button.font", "Arial 12")
+    window.option_add("*TButton.font", "Arial 12")
     window.option_add("*Text.font", "Arial 12")
-    window.option_add("*Text.background", "#ffffff")
-    window.option_add("*Text.foreground", "#333333")
+    window.option_add("*Text.background", "#2d2d2d")
+    window.option_add("*Text.foreground", "#f0f0f0")
     window.option_add("*Text.wrap", "word")
-    window.option_add("*Entry.font", "Arial 12")
+    window.option_add("*TEntry.font", "Arial 12")
 
     # Create a button for browsing files
-    # Example of improved button styling
-    browse_button = tk.Button(window, text="Browse", command=browse_file, relief=tk.RAISED, bg="#f0f0f0", padx=10, pady=5)
-    browse_button.pack(pady=10)
+    browse_button = ttk.Button(window, text="Browse", command=browse_file)
+    browse_button.pack(pady=20)
 
     # Create a chat window using a text widget
-    chat_window = tk.Text(window, height=20, width=60)
-    chat_window.pack(expand=True, fill=tk.BOTH)
+    chat_window = tk.Text(window, height=20, width=60, bg="#2d2d2d", fg="#f0f0f0", padx=10, pady=10)  # Added padding inside the widget
+    chat_window.pack(padx=10, pady=10, expand=True, fill=tk.BOTH)
     chat_window.tag_configure("bold", font=("Arial", 12, "bold"))
     chat_window.tag_configure("green", foreground="#008000")
 
     # Create an input box in the chat window
-    input_box = tk.Entry(window)
-    input_box.pack(side=tk.BOTTOM, fill=tk.X)
+    input_box = ttk.Entry(window)
+    input_box.pack(side=tk.BOTTOM, fill=tk.X, padx=20, pady=10)  # Added padding inside the widget
+    input_box.insert(0, 'Enter your message...')  # Placeholder text
+    input_box.bind("<FocusIn>", clear_placeholder)  # Bind the clear_placeholder function to focus event
 
-    # Apply a theme
-    style = ThemedStyle(window)
-    style.theme_use("clam")  # Example theme name
     style.configure("TEntry", padding=10)
 
     # Example of text widget scrollbar configuration
-    scrollbar = tk.Scrollbar(chat_window)
+    scrollbar = ttk.Scrollbar(chat_window)
     scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
     chat_window.config(yscrollcommand=scrollbar.set)
     scrollbar.config(command=chat_window.yview)
